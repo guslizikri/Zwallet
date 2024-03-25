fbfe218051e96ecc30692da784ffc4db6343f97e
Roisfaozi <roisfaozi55@gmail.com>
2024-03-25 23:59:49 +0700

    Feat: modify header component to imporve with design system

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from 'react'
 
-import Header from "../../component/Header";
-import Sidebar from "../../component/Sidebar";
+import Header from '../../component/Header'
+import Sidebar from '../../component/Sidebar'
 
 function Home() {
   return (
-    <div className="">
+    <div className=''>
       <Header />
-      <section className="container flex">
+      <section className='container flex'>
         <Sidebar />
         <main>
           <h1>Hello World</h1>
         </main>
       </section>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
