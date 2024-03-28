0d3bfd4bafc7d0eaa487210bb71778765f6c181c
saudahhasibuan <hrptaufik42@gmail.com>
2024-03-28 23:56:44 +0700

    finishing

--- src/store/reducer/user.js
+++ src/store/reducer/user.js
@@ -3,9 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'users',
   initialState: {
-    isAuth: true,
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzExNTgwNzMwLCJleHAiOjE3MTE2NjcxMzB9.LZS3my70ugkTjt6Pibifbrb0PQK3tF8UlCxgnZuZz0I',
+    isAuth: false,
+    token:'',
   },
   reducers: {
     login(state, actions) {
