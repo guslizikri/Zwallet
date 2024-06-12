2a024bea76cc5b0b682f7a77993a467eaadf2deb
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-12 22:23:59 +0700

    Revisi Landing Page & Home

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -24,37 +24,39 @@ const Carousel = ({
     <>
       <div className="overflow-hidden relative">
         <div
-          className="w-[3952px] flex transition-transform ease-out duration-500"
+          className="w-[1500px] md:w-[2400px] xl:w-[3952px] flex transition-transform ease-out duration-500"
           style={{ transform: `translateX(-${curr * 25}%)` }}
         >
           {slides}
         </div>
 
         {/* Indicator */}
-        <div className="absolute bottom-4 right-0 left-0">
+        <div className="absolute bottom-0 right-0 left-0">
           <div className="flex items-center justify-center gap-2">
             {slides.map((_, i) => (
               <div
                 key={i}
+                onClick={() => setCurr(i)}
                 className={`
-              transition-all w-3 h-3 bg-white rounded-full
-              ${curr === i ? 'p-2' : 'bg-opacity-50'}
-            `}
+              transition-all size-3 bg-primary rounded-full
+              ${curr === i ? 'p-2' : 'bg-opacity-50'}`}
               />
             ))}
           </div>
         </div>
       </div>
-      <div className="absolute inset-y-[50%] inset-x-[-120px] flex items-center justify-between p-4">
+
+      {/* Button Left and Right */}
+      <div className="absolute inset-y-[50%] inset-x-[-5px] md:inset-x-[-90px] xl:inset-x-[-120px] flex items-center justify-between p-4">
         <button
           onClick={prev}
-          className="size-[60px] rounded-[20px] shadow bg-white/80 text-gray-800 hover:bg-white flex justify-center items-center"
+          className="size-[30px] md:size-[60px] rounded-[20px] shadow bg-white/80 text-gray-800 hover:bg-white flex justify-center items-center"
         >
           <img src={prevCarousel} alt="prev carousel" />
         </button>
         <button
           onClick={next}
-          className="size-[60px] rounded-[20px] shadow bg-white/80 text-gray-800 hover:bg-white flex justify-center items-center"
+          className="size-[30px] md:size-[60px] rounded-[20px] shadow bg-white/80 text-gray-800 hover:bg-white flex justify-center items-center"
         >
           <img src={nextCarousel} alt="next carousel" />
         </button>
